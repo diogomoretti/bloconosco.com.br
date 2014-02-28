6d216947c02871eb2d45f35ce59546953930559c
Diogo Moretti <diogo.pearljam@gmail.com>
2014-02-27 21:05:51 -0300

    Change hashtag to bloconosco

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ function didLoadInstagram(event, response) {
 $('.instagram').on('didLoadInstagram', didLoadInstagram);
 
 $('.instagram').instagram({
-    hash: 'imarui',
+    hash: 'bloconosco',
     count: 20,
     clientId: 'f21ae0f72aec4d1a8ad74045e6fb7a8f'
 });
